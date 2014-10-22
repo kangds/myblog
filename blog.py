b03d3893f027ff64f434cd35c782ca97294a64b7
kangds <kangdosheng@163.com>
2014-10-22 18:17:45 +0800

    终于修改的功能齐全了

--- blog.py
+++ blog.py
@@ -1,9 +1,10 @@
+# -*- coding: UTF-8 -*-
+
 import os.path
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import time
 
 from pymongo import Connection
 
@@ -12,88 +13,90 @@ define("port", default=8000, help="run on the given port", type=int)
 
 
 class Application(tornado.web.Application):
-    def _init_(self):
+    def __init__(self):
         handlers = [
         (r'/', FirstHandler),
-         (r'/index', IndexHandler),
-          (r'/article', CommitHandler),
-          ]
-        settings = dicts(
+        (r'/index', IndexHandler),
+        (r'/article', CommitHandler),
+        ]
+        settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), ""),
-             static_path=os.path.join(os.path.dirname(__file__), "static"),
-             debug=Ture,
-             )
-        conn = Connection('localhost',27017) ,
-        self.db =conn["noun1"],
-        tornado.web.Application._int_(self,handlers,**settings)
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,        
+     ) 
+     #定义settings字典
+
+        #conn = Connection("localhost",27017) ,
+        conn = Connection()
+        self.db = conn ["blog"]
+        #self.db.blog =conn["noun1"]
+        tornado.web.Application.__init__(self,handlers,**settings)
         
     
-
-
 class FirstHandler(tornado.web.RequestHandler):           
     def get(self):
-        coll = self.application.db.article
-        article_doc = coll.find_one({"title":"noun1"})
-        noun1 = self.get_argument('noun1','')
-        blog1 = self.get_argument("blog1")
-        commit = self.get_argument('commit')
-        total = time.time() 
-        if article_doc:
-            #del article_doc["_id"]:
-            self.write(article_doc)
-        else:
-            self.self_status(404)  
+        coll = self.application.db.blog
+        article_doc = coll.find()
+        self.render('first.html',article_doc=article_doc)
 
-        self.render('first.html',title=noun1, article=blog1,commit=commit,total=time)
     def post(self):
         noun1 = self.get_argument('noun1','')
-        blog1 = self.get_argument("blog1")
-        commit = self.get_argument('commit') 
-        coll = self.application.db.article
+        blog1 = self.get_argument("blog1",'')
+        commit = self.get_argument('commit','') 
+        coll = self.application.db.blog
         article_doc = coll.find_one({"title":"noun1"})
         if article_doc:
-                 article_doc.blog1 = blog1
+                 article_doc.article = blog1
                  article_doc.title = noun1
                  article_doc.commit = commit
                  coll.save(article_doc)
         else:
-                 article_doc = {'title':noun1,'blog1':blog1,'commit':commit}
+                 article_doc = {'title':noun1,'article':blog1,'commit':commit}
                  coll.insert(article_doc)
-                 #del article_doc["_id"]:
+                 del article_doc["_id"]
                  self.write(article_doc)     
-        self.render('first.html',title=noun1, article=noun2,commit= commit,)            
+        self.render('first.html',title=noun1, article=blog1,commit= commit,)            
                 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
+        coll = self.application.db.blog
+        article_doc = coll.find_one({"title":"noun1"})
         blog1 = self.get_argument('blog1','')
         noun1 = self.get_argument('noun1','')
-        self.render('index.html')
-        db.blog.updata({"commtent":commtent},blog)
+        self.render('index.html',title=noun1,article=blog1)
     def post(self):
-        self.render('index.html')
+        coll = self.application.db.blog
+        article_doc = coll.find_one({"title":"noun1"})
+        blog1 = self.get_argument('blog1','')
+        noun1 = self.get_argument('noun1','')
+        self.render('index.html',title=noun1,article=blog1)
 
 
 class CommitHandler(tornado.web.RequestHandler):
     def post(self):
         noun1 = self.get_argument('noun1','')
         blog1 = self.get_argument('blog1','')
-        coll = self.application.db.article
-        article_doc = coll.find_one({"title":"noun1"})
+        commit = self.get_argument('commit','')
+        coll = self.application.db.blog
+        article_doc = coll.find_one({"title":noun1})
         if article_doc:
-                 article_doc.blog1 = blog1
-                 article_doc.title = title
+                 article_doc['article'] = blog1
+                 article_doc['title'] = noun1
+                 article_doc ['commit'] = commit
                  coll.save(article_doc)
+                 print coll
+                 self.redirect('/')
         else:
-                 article_doc = {'title':noun1,'blog1':blog1}
+                 article_doc = {'title':noun1,'article':blog1,'commit':commit}
                  coll.insert(article_doc)
-                 del article_doc["_id"]
-                 self.write(article_doc) 
-                 self.render('article.html', title=noun1, article=noun2, )
+                 # del article_doc["_id"]
+                 self.render('article.html', title=noun1, article=blog1,commit=commit )
     def get(self):
         noun1 = self.get_argument('noun1','')
         blog1 = self.get_argument('blog1','')
-        self.render('article.html', title=noun1, article=noun2, )
+        commit = self.get_argument('commit','')
+        self.render('article.html', title=noun1, article=blog1, commit=commit)
 
 if __name__ == '__main__':
     print ('systerm started ...')
